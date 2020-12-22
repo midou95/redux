64e6d067d889d70c3c906dff881b016117affa5c
midou95 <midouchecha@gmail.com>
2020-12-22 20:41:51 +0100

    my redux proj

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+import List from "./Components/List";
+import Add from "./Components/Add";
+
+function App() {
+  return (
+    <div className="App">
+      <Add />
+      <List />
+      
+    </div>
+  );
+}
+
+export default App;
